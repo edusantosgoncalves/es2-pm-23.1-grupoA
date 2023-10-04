a21b94b69ca100e442b757fa1b41e4782431d266
Vivian Ferraro <55963058+viferraro@users.noreply.github.com>
2023-10-04 16:30:11 -0300

    Update server.py
    edicao de teste

--- server.py
+++ server.py
@@ -8,8 +8,4 @@ app = Flask(__name__)
 def inicio():
     return render_template('index.html')
 
-@app.route('/inputs')
-def novo():
-    return render_template('inputs.html', titulo='Inputs')
-
 app.run()
